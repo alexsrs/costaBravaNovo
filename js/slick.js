2b387dd48011848de189817587e9be25dcf9abea
Alex Sandro <alex@creapix.com.br>
2019-10-28 11:48:49 -0300

    falta pouco

---
+++ js/slick.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+ 
+	$('.aconteceu-wrapper').slick({
+	 	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	autoplay: true,
+	  	autoplaySpeed: 3000,
+	  	dots: true
+
+	});
+});
+
+
